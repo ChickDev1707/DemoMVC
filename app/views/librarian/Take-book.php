d9a8819141503a3bf67ce817854982e269ec932b
HoangKhang0410 <khongnoihailan@gmail.com>
2021-04-25 09:58:47 +0700

    Add branch BookTaking

--- app/views/librarian/Take-book.php
+++ app/views/librarian/Take-book.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href= "<?php echo URLROOT;?>public/css/Main.css">
     <link rel="stylesheet" href= "<?php echo URLROOT;?>public/css/librarian/Take-book.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Document</title>
 </head>
 <body>
@@ -14,7 +15,74 @@
         require APPROOT."/views/includes/Librarian-nav-panel.php";
     ?>
     <div id="take-book-panel">
-        
+        <div class="book-form">
+            <div class="tittle">
+                <div class="tittle-picture">
+                    <img src="https://www.thechildrensbookreview.com/wp-content/uploads/2010/11/Magic-Book-Featured-Image.jpg" alt="">
+                </div>
+                <div class="tittle-content">
+                    <h2>Nhập sách ở đây nè !</h2>
+                    <i class="fas fa-hand-point-right"></i>
+                </div>
+            </div>
+            <div class="form">
+                <form method = "POST">
+                    <div class = "info-field">
+                        <h3>Mã sách</h3>
+                        <div class="input-field">
+                            <input type="text" placeholder = "Book's code" name = "book_code" required>
+                            <i class="fas fa-qrcode"></i>
+                        </div>
+                    </div>
+                    <div class = "info-field">
+                        <h3>Tên sách</h3>
+                        <div class="input-field">
+                            <input type="text" placeholder = "Book's name" name = "book_name" required>
+                            <i class="fas fa-book"></i>
+                        </div>
+                    </div>
+                    <div class = "info-field">
+                        <h3>Thể loại</h3>
+                        <div class="input-field">
+                            <input type="text" placeholder = "Type" name = "book_type" required>
+                            <i class="fas fa-book"></i>
+                        </div>
+                    </div>
+                    <div class = "info-field">
+                        <h3>Tác giả</h3>
+                        <div class="input-field">
+                            <input type="text" placeholder = "Author" name = "book_author" required>
+                            <i class="fas fa-book-reader"></i>
+                        </div>
+                    </div>
+                    <div class = "info-field">
+                        <h3>Năm xuất bản</h3>
+                        <div class="input-field">
+                            <select name="book_year" required>
+                                <?php
+                                    for ($year = (int)date('Y'); 1950 <= $year; $year--): ?>
+                                        <option value="<?=$year;?>"><?=$year;?></option>
+                                <?php endfor; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class = "info-field">
+                        <h3>Ngày nhập</h3>
+                        <div class="input-field">
+                            <input name="book_import" type="date" value="<?php echo date('Y-m-j'); ?>" required>
+                        </div>
+                    </div>
+                    <div class = "info-field">
+                        <h3>Trị giá</h3>
+                        <div class="input-field">
+                            <input type="number" placeholder = "Cost" name = "book_cost" required>
+                            <i class="fas fa-money-bill-wave"></i>
+                        </div>
+                    </div>
+                    <input type="submit" value = "ADD" name = "submit">
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>

066cfbaf3a052c1b7242e4042ea0fa45495b3f1b
sonlam <19522133@gm.uit.edu.vn>
2021-05-29 07:17:15 +0700

    update return book

--- app/controllers/ReturnBook.php
+++ app/controllers/ReturnBook.php
@@ -8,26 +8,34 @@
         public function index () {
             $this->view('librarian/ReturnBook');
             
-            if(isset($_POST['returnBook'])) {
-                $borrowedDate = $this->returnbookModel->findDateBorrowed($_POST['borrowCardId']);
-                $expriedDaysBorrowed = $this->expriedDaysBorrowed($this->formatDate($borrowedDate->NGAY_MUON), $_POST['datereturn']);
+            if(isset($_POST['submit_lend_book'])) {
+                $borrowedDate = $this->returnbookModel->findDateBorrowed($_POST['rb_card_id']);
+                $expriedDaysBorrowed = $this->expriedDaysBorrowed($this->formatDate($borrowedDate->NGAY_MUON), $_POST['rb_date']);
 
                 if($expriedDaysBorrowed < 0) {
                     $ErrMessage = "Invalid Date!";
                     echo $ErrMessage;
                     $this->showErrorMessage($ErrMessage);
+                } else if($expriedDaysBorrowed <= 4) {
+                    $data = [
+                        'ma_phieu_muon_tra'=>$_POST['rb_card_id'],
+                        'ngay_tra'=>$_POST['rb_date']
+                    ];
+                    $this->returnbookModel->updateBookCard($_POST['rb_card_id']);
+                    $this->returnbookModel->updateReturnCard($data);
                 } else {
 
-                    $borrowedDate = $this->returnbookModel->findDateBorrowed($_POST['borrowCardId']);
+                    $borrowedDate = $this->returnbookModel->findDateBorrowed($_POST['rb_card_id']);
                     $TienPhatMoiNgay = $this->returnbookModel->getTienPhatMoiNgay();
 
                     $data = [
-                        'ngay_tra'=>$_POST['datereturn'],
+                        'ma_phieu_muon_tra'=>$_POST['rb_card_id'],
+                        'ngay_tra'=>$_POST['rb_date'],
                         'so_ngay_tra_tre'=>$expriedDaysBorrowed,
                         'tien_phat_ky'=>$this->punishMoney($expriedDaysBorrowed, $TienPhatMoiNgay)
                     ];
-                    $this->returnbookModel->updateBookCard($_POST['borrowCardId']);
-                    $this->returnbookModel->updateReturnCard($data);
+                    $this->returnbookModel->updateBookCard($_POST['rb_card_id']);
+                    $this->returnbookModel->updateReturnCardLate($data);
                 }
                 
             }

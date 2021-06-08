ba9a9f49add6650bca932911a577eefbc786b52c
HoangKhang0410 <khongnoihailan@gmail.com>
2021-06-08 14:39:22 +0700

    Add Search By Selection and A little Css

--- public/js/Book-searching.js
+++ public/js/Book-searching.js
@@ -1,17 +1,36 @@
 const list = document.querySelector('.book-list');
 const searchBar = document.querySelector('.search-bar input');
+const searchBtn = document.querySelector('.search-bar button');
+let selectBox = document.querySelector('.search-bar select');
+let selection = selectBox.options[selectBox.selectedIndex].value; // lấy value trong option
 
-searchBar.addEventListener('keyup', function(e){
-    const searchString = e.target.value.toLowerCase();
-    const filterBooks = data.filter(book => {
-        return book["TEN_SACH"].toLowerCase().includes(searchString) || book['TAC_GIA'].toLowerCase().includes(searchString);
-    });
-    displayBooks(filterBooks);
-})
 
 window.addEventListener('DOMContentLoaded', function(){
     displayBooks(data['books']);
 })
+// cập nhật giá trị select
+selectBox.addEventListener('change', function(){
+    selection = selectBox.options[selectBox.selectedIndex].value;
+    console.log(selection);
+})
+
+// Tìm kiếm với giá trị tương ứng
+searchBar.addEventListener('keyup', function(e){
+    const searchString = e.target.value.toLowerCase();
+    searchBtn.addEventListener('click', function(){
+        const filterBooks = data['books'].filter((book) => {
+            if (selection == "book_name") return book['TEN_SACH'].toLowerCase().includes(searchString);
+            else if (selection == "book_author") return book['TAC_GIA'].toLowerCase().includes(searchString);
+            else if (selection == "book_type") return book['THE_LOAI'].toLowerCase().includes(searchString);
+            else return ( book['TEN_SACH'].toLowerCase().includes(searchString) ||
+                          book['TAC_GIA'].toLowerCase().includes(searchString)  ||
+                          book['THE_LOAI'].toLowerCase().includes(searchString));
+        });
+        displayBooks(filterBooks);
+    })
+})
+
+
 function displayBooks(data){
     let books = data.map(function(item){
         return `<div class="book-container">
@@ -19,7 +38,11 @@ function displayBooks(data){
         <div class="info-box">
             <h3><i class="far fa-bookmark"></i> ${item['TEN_SACH']}</h3>
             <p><i class="far fa-user"></i> ${item['TAC_GIA']}</p>
-            <div class="status"> ${item['TINH_TRANG'] == false ? "Borrow" : "Borrowed"}</div>
+            <div class="status" style = "background-color:${item['TINH_TRANG'] == false ? "green" : "red"}"> ${item['TINH_TRANG'] == false ? "Borrow" : "Invalid"}</div>
+            <div class="detail-and-update">
+                <button id ="detail" onclick=showDetailPanel()>Detail</button>
+                <button id="update">Update</button>
+            </div>
         </div>
     </div>`
     })

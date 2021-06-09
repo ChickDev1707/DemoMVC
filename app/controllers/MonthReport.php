31852c68c9b00ff050ef14a09d02c759fef1d8cf
sonlam <19522133@gm.uit.edu.vn>
2021-06-09 21:31:16 +0700

    fix type of reader

--- app/controllers/MonthReport.php
+++ app/controllers/MonthReport.php
@@ -10,40 +10,54 @@
         public function index() {
             
             if(isset($_POST['mr_create_report'])) {
-                $dataMonthYear = [
-                    'thang'=>$_POST['month'],
-                    'nam'=>$_POST['year']
-                ];
+                $message = "";
+                $type = "correct";
+
                 $dataExport = $this->MonthReportModel->exportReport($_POST['month'], $_POST['year']);
-                $rows = $this->MonthReportModel->getAllMonthYear();
-                $total =$this->MonthReportModel->countRows($_POST['month'], $_POST['year']);
+                if($dataExport == null) {
+                    $message = "Không có lượt mượn nào trong tháng";
+                    $type = "incorrect";
 
-                $dataReport = [
-                    'month'=>$_POST['month'],
-                    'year'=>$_POST['year'],
-                    'total'=>$total
-                ];
+                    $this->view("librarian/Month-report");
 
-                if($this->checkData($rows, $_POST['month'], $_POST['year'])) { // chua ton tai
-                    $this->MonthReportModel->createMonthReport($dataReport);
-                    $MonthReportId = $this->MonthReportModel->getMonthReportId($_POST['month'], $_POST['year']);
-                    foreach($dataExport as $e) {
-                        $dataDetails = [
-                            'id'=>$MonthReportId,
-                            'type'=>$e->THE_LOAI,
-                            'total'=>$e->TOTAL,
-                            'rate'=>$e->TOTAL/$total
-                        ];
-                        $this->MonthReportModel->createMonthReportDetails($dataDetails);
+                } else {
+                    $rows = $this->MonthReportModel->getAllMonthYear();
+                    $total =$this->MonthReportModel->countRows($_POST['month'], $_POST['year']);
+
+                    $dataReport = [
+                        'month'=>$_POST['month'],
+                        'year'=>$_POST['year'],
+                        'total'=>$total
+                    ];
+
+                    if($this->checkData($rows, $_POST['month'], $_POST['year'])) { // chua ton tai
+
+                        $this->MonthReportModel->createMonthReport($dataReport);
+                        $MonthReportId = $this->MonthReportModel->getMonthReportId($_POST['month'], $_POST['year']);
+                        foreach($dataExport as $e) {
+                            $dataDetails = [
+                                'id'=>$MonthReportId,
+                                'type'=>$e->THE_LOAI,
+                                'total'=>$e->TOTAL,
+                                'rate'=>$e->TOTAL/$total  
+                            ];
+                            $this->MonthReportModel->createMonthReportDetails($dataDetails);
+                        }
+                        $data = $this->MonthReportModel->getData($_POST['month'], $_POST['year']);
+                        $this->view("librarian/Month-report", $data);
                     }
-                    $data = $this->MonthReportModel->getData($_POST['month'], $_POST['year']);
-                    $this->view("librarian/Month-report", $data);
+                    // da ton tai
+                    else {
+                        $data = $this->MonthReportModel->getData($_POST['month'], $_POST['year']);
+                        $this->view("librarian/Month-report", $data);
+                    }
+
                 }
-                // da ton tai
-                else {
-                    $data = $this->MonthReportModel->getData($_POST['month'], $_POST['year']);
-                    $this->view("librarian/Month-report", $data);
-                }
+                $vars = array($type, $message);
+                $jsVars = json_encode($vars, JSON_HEX_TAG | JSON_HEX_AMP);
+                    
+                echo "<script> showMessageBox.apply(null, $jsVars);</script>";
+                
             } else {
                 $this->view("librarian/Month-report");
             }
@@ -55,6 +69,6 @@
             }
             return true;
         }
-        
+
     }
 ?>

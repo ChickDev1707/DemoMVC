b28d592131fd8adb11167affb00aa5b3e72134c6
ChickDev1707 <thuancoixy234786@gmail.com>
2021-06-11 21:59:22 +0700

    update reader card - add tabs

--- app/views/librarian/Reader-card.php
+++ app/views/librarian/Reader-card.php
@@ -27,62 +27,115 @@
     </div>
     <!-- header -->
     <div class="feature-panel-wrapper">
-        <div class="feature-panel" id="reader-card-panel">
-        <div class="info-form-outer">
-                <div class="left-box">
-                    <div class="title-icon"><i class="fas fa-book-reader"></i></div>
-                    <div>
-                        <h3>Tạo thẻ độc giả</h3>
-                        <p>Nhập vào thông tin cá nhân theo form bên cạnh, lưu ý nhập thông tin hợp lệ theo yêu cầu</p>
-                    </div>
-                    <div class="right-arrow"><i class="fas fa-angle-right"></i></div>
-                </div>
-                <form method="POST" class="right-form">
-                    <div class = "info-field">
-                        <label for="">Tên độc giả</label>
-                        <div class="input-field">
-                            <input type="text" placeholder = "Họ và tên" name = "name" required>
-                        </div>
-                    </div>
-                    <div class = "info-field">
-                        <label for="">Loại độc giả</label>
-                        <div class="input-field">
-                            <select name="type_of_Reader" id="">
-                                <?php foreach($data as $author) { ?>
-                                    <option value="<?php echo $author->TEN_LOAI_DOC_GIA ?>"><?php echo $author->TEN_LOAI_DOC_GIA ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class = "info-field">
-                        <label for="">Ngày sinh</label>
-                        <div class="input-field">
-                            <input type="date" placeholder = "Date of birth" name = "date_of_birth" required>
-                        </div>
-                    </div>
-                    <div class = "info-field">
-                        <label for="">Địa chỉ</label>
-                        <div class="input-field">
-                            <input type="text" placeholder = "Địa chỉ" name = "address" required>
-                        </div>
-                    </div>
-                    <div class = "info-field">
-                    <label for="">Email</label>
-                        <div class="input-field">
-                            <input type="text" placeholder = "Email" name = "email" required>
-                        </div>
-                    </div>
-                    <div class = "info-field">
-                        <label for="">Ngày lập thẻ</label>
-                        <div class="input-field">
-                            <input name="book_import" type="date" required>
-                        </div>
-                    </div>
-                    <input type="submit" value = "Tạo thẻ" name = "submit">
-                </form>
-            </div>
-            <?php require APPROOT."/views/includes/Message-box.php"; ?>
+        <div class="feature-panel tab-bar-panel" id="reader-card-panel">
+        <input type="radio" id="create-reader-card-tab" class= "tab-1" name="tabs" checked>
+        <input type="radio" id="reader-card-list-tab" class= "tab-2" name="tabs">
+        <div class="nav-container">
+            <nav>
+                <label class="lable-tab-1" for="create-reader-card-tab"><i class="far fa-plus-square"></i> Tạo thẻ</label>
+                <label class="lable-tab-2" for="reader-card-list-tab"><i class="far fa-list-alt"></i> Danh sách</label>
+                <div class="slider"></div>
+            </nav>
         </div>
+        <section>
+            <div class="content content-1">
+                <div class="info-form-outer">
+                    <div class="left-box">
+                        <div class="title-icon"><i class="fas fa-book-reader"></i></div>
+                        <div>
+                            <h3>Tạo thẻ độc giả</h3>
+                            <p>Nhập vào thông tin cá nhân theo form bên cạnh, lưu ý nhập thông tin hợp lệ theo yêu cầu</p>
+                        </div>
+                        <div class="right-arrow"><i class="fas fa-angle-right"></i></div>
+                    </div>
+                    <form method="POST" class="right-form">
+                        <div class = "info-field">
+                            <label for="">Tên độc giả</label>
+                            <div class="input-field">
+                                <input type="text" placeholder = "Họ và tên" name = "name" required>
+                            </div>
+                        </div>
+                        <div class = "info-field">
+                            <label for="">Loại độc giả</label>
+                            <div class="input-field">
+                                <select name="type_of_Reader" id="">
+                                    <?php foreach($data as $author) { ?>
+                                        <option value="<?php echo $author->TEN_LOAI_DOC_GIA ?>"><?php echo $author->TEN_LOAI_DOC_GIA ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class = "info-field">
+                            <label for="">Ngày sinh</label>
+                            <div class="input-field">
+                                <input type="date" placeholder = "Date of birth" name = "date_of_birth" required>
+                            </div>
+                        </div>
+                        <div class = "info-field">
+                            <label for="">Địa chỉ</label>
+                            <div class="input-field">
+                                <input type="text" placeholder = "Địa chỉ" name = "address" required>
+                            </div>
+                        </div>
+                        <div class = "info-field">
+                        <label for="">Email</label>
+                            <div class="input-field">
+                                <input type="text" placeholder = "Email" name = "email" required>
+                            </div>
+                        </div>
+                        <div class = "info-field">
+                            <label for="">Ngày lập thẻ</label>
+                            <div class="input-field">
+                                <input name="book_import" type="date" required>
+                            </div>
+                        </div>
+                        <input type="submit" value = "Tạo thẻ" name = "submit">
+                    </form>
+                </div>
+                <?php require APPROOT."/views/includes/Message-box.php"; ?>
+                </div>
+            </div>
+            <div class="content content-2">
+                <h3>Danh sách độc giả</h3>
+                <table>
+                    <tr>
+                        <th>STT</th>
+                        <th>Họ Tên</th>
+                        <th>Loại Độc giả</th>
+                        <th>Ngày sinh</th>
+                        <th>Địa chỉ</th>
+                        <th>Email</th>
+                        <th>Ngày lập thẻ</th>
+                        <th>Sửa</th>
+                        <th>Xóa</th>
+                    </tr>
+                    <tr>
+                        <form action="">
+                            <td>1</td>
+                            <td>Nhan Huu Thuan</td>
+                            <td>X</td>
+                            <td>20-11-2001</td>
+                            <td>Newyork</td>
+                            <td>thuancoixy234786@gmail.com</td>
+                            <td>27-05-2001</td>
+                            <td><button class="update-btn">Sửa</button></td>
+                            <td><button class="delete-btn">Xóa</button></td>
+                        </form>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Huu Thuan</td>
+                        <td>X</td>
+                        <td>20-11-2001</td>
+                        <td>Newyork</td>
+                        <td>thuancoixy234786@gmail.com</td>
+                        <td>27-05-2001</td>
+                        <td><button class="update-btn">Sửa</button></td>
+                        <td><button class="delete-btn">Xóa</button></td>
+                    </tr>
+                </table>
+            </div>
+        </section>
     </div>
 </body>
 </html>
